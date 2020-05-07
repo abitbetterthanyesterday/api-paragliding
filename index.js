fb9dfe1213c13632ce94a280a296a2e0aef33fab
Aloys Berger <aloysberger@gmail.com>
2020-05-07 19:22:58 +0200

    Minor coding syntax changes

--- index.js
+++ index.js
@@ -3,18 +3,21 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
-//  Set up mongoose connection
 const mongoose = require('mongoose')
 
+//  Set up mongoose connection
 const { DB_HOST, DB_PORT, DB_LOGIN, DB_PWD, DB_NAME } = process.env
+mongoose.connect(`mongodb://${DB_LOGIN}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+const db = mongoose.connection
 
-mongoose.connect(`mongodb://${DB_LOGIN}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, (err, res) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('🎊 You are connected to the database')
-  }
+// Error connection to DB
+db.on('error', (err) => {
+  console.log('DB not connected ', err)
+})
+
+// Success connection to DB.
+db.once('open', () => {
+  console.log('🎊 Hoora! You are connected to the database')
 })
 
 app.listen(3000, () => {
